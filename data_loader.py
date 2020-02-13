cefb48b2a0885f2473411d4d0f6f869c2b94cdb6
duje.medak <duje.medak@fer.hr>
2020-02-13 20:15:54 +0100

    Added data loader

---
+++ data_loader.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import os
+import json
+
+
+class DataLoader:
+    def __init__(self, config_path):
+        assert os.path.exists(config_path), "You must provide a valid json configuration file!"
+        with open(config_path) as config_file:
+            data = json.load(config_file)
+
+        self.base_path = data['base_path']
+
+    def load_csv_data(self, filename="bank.csv", load_first_n=None):
+        data_folder = os.path.join(self.base_path, "data", "bank")
+        input_csv_path = os.path.join(data_folder, filename)
+        if not os.path.exists(input_csv_path):
+            print("there is no file with this path:", input_csv_path)
+
+        print("loading data from:",input_csv_path)
+        data = pd.read_csv(input_csv_path, sep=";", nrows=load_first_n)
+        names = list(data.columns.values)
+        target_column = names[-1]
+        input_names = names[:-1]
+        X = data.loc[:, input_names]
+        print(X)
+        y = data.loc[:, target_column]
+        print(y)
+        return X, y
